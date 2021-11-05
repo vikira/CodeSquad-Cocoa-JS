695d232fa86ce9a40552d6a23ebb2098dd3a944b
Yoojin Yang <y29@naver.com>
2021-11-05 11:58:40 +0900

    2-3

---
+++ Mission/2-21.11.03/2-3.meanScore.js
@@ -0,0 +1,35 @@
+//Mission2 배열다루기
+//3. 평균 구하기
+
+const grades = [
+  [88, 76, 77],
+  [33, 44, 44],
+  [90, 100, 94],
+  [30, 44, 98],
+];
+
+function getAvg(grades) {
+  let avgScore = 0; //각 학생의 평균 점수
+  let maxAvgScore = 0; //모든 학생의 최고점수의 평균 점수
+  let tmp = [];
+  let numOfStudents = grades.length;
+  let numOfSubjects = grades[0].length;
+
+  for (let i = 0; i < numOfStudents; i++) {
+    for (let j = 0; j < numOfSubjects; j++) {
+      avgScore += grades[i][j];
+    }
+    //(1) 각 학생의 평균점수 구하기
+    avgScore /= numOfSubjects;
+    console.log(i + 1, '번 학생 평균 점수 : ', avgScore.toFixed(2)); //소수 2째자리까지 반올림
+    avgScore = 0; //초기화
+
+    //(2) 각 학생의 최고 점수 구하기
+    maxAvgScore += Math.max(...grades[i]);
+  }
+  //(2) 모든 학생의 최고 점수의 평균 구하기
+  maxAvgScore /= numOfStudents;
+  console.log('모든 학생의 최고점수의 평균점수 : ', maxAvgScore);
+}
+
+getAvg(grades);
