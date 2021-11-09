4f85364cbb2e9788a60fac51a427616f9ee16f8f
Yoojin Yang <y29@naver.com>
2021-11-10 01:03:23 +0900

    mission1 edit

--- Mission/1-21.11.01/getPolyArea.js
+++ Mission/1-21.11.01/getPolyArea.js
@@ -1,70 +1,71 @@
 //[Mission 1] 다각형의 넓이 구하기
 //1.getArea 함수를 만들어서 모든 도형의 넓이를 구한다.
 
-let orderArr = [];
+let sequenceArr = [];
 
 //다각형 종류 확인
-function getArea(shape, ...size) {
-  switch (shape) {
+function getArea(type, ...size) {
+  switch (type) {
     case 'circle':
-      circleArea(...size);
+      area = getCircArea(...size);
       break;
 
     case 'rect':
-      rectArea(...size);
+      area = getRectArea(...size);
       break;
 
-    case 'trapezoid':
-      trapezArea(...size);
+    case 'trap':
+      area = getTrapArea(...size);
       break;
 
     default:
       return false;
   }
+  saveExecutionSequence(type, area);
 }
 
 //circle
-function circleArea(size) {
+function getCircArea(...size) {
   if (size.length === 1) {
-    console.log(Math.PI * size[0] * size[0]);
-    return Math.PI * size[0] * size[0];
+    circArea = Math.PI * size * size;
+    return circArea;
   }
 
   let sum = 0;
-  for (let i = size[0]; i < size[1]; i++) {
-    sum += size[0] * size[0] * Math.PI;
+  for (let i = size[0]; i <= size[1]; i++) {
+    sum += i * i * Math.PI;
   }
   return sum;
 }
 
 //rectangle
-function rectArea(height, width) {
-  console.log(height * width);
-  return height * width;
+function getRectArea(width, height) {
+  let rectArea = height * width;
+  return rectArea;
 }
 
 //trapezoid
-function trapezArea(top, bottom, height) {
-  console.log(((top + bottom) * height) / 2);
-  return ((top + bottom) * height) / 2;
+function getTrapArea(top, bottom, height) {
+  trapArea = ((top + bottom) * height) / 2;
+  return trapArea;
 }
 
 //2. printExecutionSequence 함수 만들기
-//호출된 함수의 순서와 실행결과를 stack에 push
-function saveExecutionSequence(shape, returnedData) {
-  orderArr.push(`${type} : ${returnedData}`);
+//호출된 함수의 순서와 실행결과를 sequenceArr에 push
+function saveExecutionSequence(type, area) {
+  sequenceArr.push(`${type} : ${area}`);
 }
 
 //호출된 함수의 순서와 실행 결과 출력
 function printExecutionSequence() {
-  let num = 0;
-  for (let i = 0; i < orderArr.length; i++) {
-    console.log();
-  }
+  sequenceArr.forEach((element) => {
+    console.log(element);
+  });
 }
 
 //test case
-getArea('circle', 10);
-getArea('rect', 10, 15);
-getArea('trapezoid', 10, 15, 12);
-getArea('circle', 1, 5);
+getArea('circle', 10); //314.159...
+getArea('rect', 10, 15); //150
+getArea('trap', 10, 15, 12); //150
+getArea('circle', 1, 5); //172.787...
+printExecutionSequence();
